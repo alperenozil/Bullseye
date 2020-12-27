0e9b8343a4c47e213d6799046b64dc065b94a0c6
alperenozil <necdet.ozil@hacettepe.edu.tr>
2020-12-27 18:40:06 +0300

    Update ContentView.swift

--- Bullseye/ContentView.swift
+++ Bullseye/ContentView.swift
@@ -3,18 +3,20 @@ struct ContentView: View {
     @State var alertIsVisible:Bool=false
     var body: some View {
         VStack {
+            Spacer()
             // Target row
             HStack {
                 Text("Put the bullseye as close as you can to:")
                 Text("100")
             }
+            Spacer()
             // Slider row
             HStack {
                 Text("1")
                 Slider(value: .constant(10))
                 Text("100")
             }
-            
+            Spacer()
             // Button row
             Button(action: {
                 print("button pressed")
@@ -26,7 +28,23 @@ struct ContentView: View {
                 ()->Alert
                 in return Alert(title: Text("Joke"), message: Text("Temel ucaktan atlamıs dunyayı tutturamamis"), dismissButton: .default(Text("Gapath")))
             }
+            Spacer()
             // Score row
+            HStack{
+                Button(action: {}){
+                    Text("Start Over")
+                }
+                Spacer()
+                Text("Score: ")
+                Text("999")
+                Spacer()
+                Text("Round: ")
+                Text("999")
+                Spacer()
+                Button(action: {}){
+                    Text("Info")
+                }
+            }.padding(.bottom, 24)
         }
     }
 }
