b0ba2bf9e37a8d0b1cdf508b62405360e26b10c5
alperenozil <necdet.ozil@hacettepe.edu.tr>
2020-12-27 13:42:44 +0300

    Update ContentView.swift

--- Bullseye/ContentView.swift
+++ Bullseye/ContentView.swift
@@ -3,24 +3,26 @@ struct ContentView: View {
     @State var alertIsVisible:Bool=false
     var body: some View {
         VStack {
+            // Target row
             Text("Welcome to my first iOS app!")
                 .foregroundColor(Color.purple)
                 .padding()
+            // Slider row
             Button(action: {
                 print("button pressed")
                 self.alertIsVisible=true
             }) {
-                Text("I am a button")
+                Text("knock knock")
             }
             .alert(isPresented: $alertIsVisible){
                 ()->Alert
-                in return Alert(title: Text("Adın ne olm"), message: Text("Alperen mi?"), dismissButton: .default(Text("Gapath")))
+                in return Alert(title: Text("Joke"), message: Text("Temel ucaktan atlamıs dunyayı tutturamamis"), dismissButton: .default(Text("Gapath")))
             }
         }
     }
 }
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        ContentView().previewLayout(.fixed(width: 896.0, height: 414))
     }
 }
