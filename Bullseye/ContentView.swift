2b3cc3d1a8e16ced139952f29046647d2fe34247
alperenozil <necdet.ozil@hacettepe.edu.tr>
2020-12-28 11:52:25 +0300

    Update ContentView.swift

--- Bullseye/ContentView.swift
+++ Bullseye/ContentView.swift
@@ -2,13 +2,13 @@ import SwiftUI
 struct ContentView: View {
     @State var alertIsVisible:Bool=false
     @State var sliderValue: Double=50.0
+    @State var target: Int=Int.random(in: 1...100)
     var body: some View {
         VStack {
             Spacer()
             // Target row
             HStack {
-                Text("Put the bullseye as close as you can to:")
-                Text("100")
+                Text("Put the bullseye as close as you can to: \(self.target)")
             }
             Spacer()
             // Slider row
@@ -23,11 +23,11 @@ struct ContentView: View {
                 print("button pressed")
                 self.alertIsVisible=true
             }) {
-                Text("Show value")
+                Text("Show Result")
             }
             .alert(isPresented: $alertIsVisible){
                 ()->Alert
-                in return Alert(title: Text("Value"), message: Text("Slider is at \(Int(self.sliderValue.rounded())) now"), dismissButton: .default(Text("Gapath")))
+                in return Alert(title: Text("Value"), message: Text("Slider is at \(Int(self.sliderValue.rounded())) now\n"+"Your score is \(self.pointsForCurrentRound())"), dismissButton: .default(Text("Gapath")))
             }
             Spacer()
             // Score row
@@ -47,6 +47,12 @@ struct ContentView: View {
                 }
             }.padding(.bottom, 24)
         }
+    }
+    func pointsForCurrentRound()->Int {
+        let roundedValue=Int(self.sliderValue.rounded())
+        let difference=abs(self.target-roundedValue)
+        let awardedPoints=100-difference
+        return awardedPoints
     }
 }
 struct ContentView_Previews: PreviewProvider {
